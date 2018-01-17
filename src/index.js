73d11a56d0f3b226c69bd60191dbc814bc49bc18
戈洋洋 <geyy@jcgroup.com.cn>
2018-01-17 14:12:05 +0800

    chore: upgrade react to 16.2.0

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ function initComponent() {
 
 initComponent()
 
-// expose React and Perf for __DEV__
+// expose React for __DEV__
 if (__DEV__) {
-  const Perf = require('react-addons-perf') // eslint-disable-line global-require, import/no-extraneous-dependencies
-
   window.React = React
-  window.Perf = Perf
 }
